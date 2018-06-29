155c6ca184e0236236cc3c783549faaf9781057b
Martin Nnachi <mnnachi@gmail.com>
2018-06-29 21:58:55 +0100

    first commit

---
+++ src/com/martinnnachi/AnotherThread.java
@@ -0,0 +1,19 @@
+package com.martinnnachi;
+
+import static com.martinnnachi.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(ANSI_BLUE + "Another thread woke me up");
+        }
+
+        System.out.println(ANSI_BLUE + "Three seconds havre passed. I'm awake.");
+    }
+
+}
