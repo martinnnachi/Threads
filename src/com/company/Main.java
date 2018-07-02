c0d8c9ff08fda92d3554eca3b86128fb20ca0231
martinnnachi <mnnachi@gmail.com>
2018-07-02 16:25:59 +0100

    Second commit on threads

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,22 +9,24 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(ANSI_PURPLE + "Hello from the main thread");
-        Thread anotherThread = new AnotherThread();
+        final Thread anotherThread = new AnotherThread();
         anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
-        new Thread() { //anonymous class
-
-            public void run() {
-                System.out.println(ANSI_GREEN + "Hello from the anonymous class Thread");
-            }
-        }.start();
+        //anonymous class
+        new Thread(() -> System.out.println(ANSI_GREEN + "Hello from the anonymous class Thread")).start();
 
         Thread myRunnableThread = new Thread(new MyRunnable() {
 
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "Another thread terminated or timed out, so i'm running again");
+                }catch (InterruptedException e){
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted");
+                }
             }
         });
         myRunnableThread.start();
